7a637ee77241c16d4ca9e9534e29cb50f4a469ae
daocuong2306 <115203956+daocuong2306@users.noreply.github.com>
2023-10-22 09:57:39 +0700

    login

--- CRUD ,API/src/js/user.js
+++ CRUD ,API/src/js/user.js
@@ -11,6 +11,26 @@ const dang_ky = () => {
     fetch('http://localhost:3000/users', {
         method: 'POST',
         headers: { "Content-Type": "application/json", },
-        body : JSON.stringify(user)
+        body: JSON.stringify(user)
     })
+}
+
+const dang_nhap = () => {
+    // b1 : lấy data được nhập
+    const email = document.querySelector('#exampleInputEmail1').value;
+    const password = document.querySelector('#exampleInputPassword1').value;
+    // b2 : lấy dứ liệu từ api.user 
+    fetch('http://localhost:3000/users')
+        .then(response => response.json())
+        .then((data) => {
+            console.log(data);
+            // b3: so sánh
+            for (let user of data) {
+                if ((user.email == email) && (user.password == password)) {
+                    window.location.href = 'dashboard.html';
+                    return;
+                }
+            }
+            alert("sai tk hoặc mk")
+        })
 }
